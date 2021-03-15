c806632fb55fa10e06b5674485d207deb2d8008b
Anthony Avila <anthony.p.avila@gmail.com>
2021-03-15 19:52:58 +0900

    Set up exercise for Arrays

---
+++ src/com/DSA/Main.java
@@ -0,0 +1,12 @@
+package com.DSA;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int[] numbers = {10, 20, 30};
+        System.out.println(numbers.length);
+        System.out.println(Arrays.toString(numbers)); // we use Arrays class to use toString method
+    }
+}
