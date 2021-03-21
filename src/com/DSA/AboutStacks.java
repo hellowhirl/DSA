327bd0f76b35d2ddb6832642e738a9cf1c39f8c1
Anthony Avila <anthony.p.avila@gmail.com>
2021-03-21 10:26:44 +0900

    my wonky solution for revering a String

---
+++ src/com/DSA/AboutStacks.java
@@ -0,0 +1,20 @@
+package com.DSA;
+
+import java.util.Stack;
+
+public class AboutStacks {
+    public static void main(String[] args) {
+        // Stack class is generic, it has angled bracks <E> so we can store any kinds of objects
+        Stack<Integer> stack = new Stack<>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        System.out.println(stack); // looks like array [ ], but it's just for presentation. Stacks behave differently
+        var top = stack.pop();
+        System.out.println("top: " + top);
+        System.out.println(stack);
+        var currentTOp = stack.peek();
+        System.out.println("currentTOp: " + currentTOp);
+        System.out.println(stack.isEmpty());
+    }
+}
--- src/com/DSA/Main.java
+++ src/com/DSA/Main.java
@@ -1,20 +1,17 @@
 package com.DSA;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-    	// Stack class is generic, it has angled bracks <E> so we can store any kinds of objects
-		Stack<Integer> stack = new Stack<>();
-		stack.push(10);
-		stack.push(20);
-		stack.push(30);
-		System.out.println(stack); // looks like array [ ], but it's just for presentation. Stacks behave differently
-		var top = stack.pop();
-		System.out.println("top: " + top);
-		System.out.println(stack);
-		var currentTOp = stack.peek();
-		System.out.println("currentTOp: " + currentTOp);
-		System.out.println(stack.isEmpty());
+    	String str = "abcd";
+    	String[] words = str.split("");
+		System.out.println(Arrays.toString(words));
+		Stack<String> reversed = new Stack<>();
+		for (int i = words.length -1; i >= 0; i--) {
+			reversed.push(words[i]);
+		}
+		System.out.println(reversed);
 	}
 }
