7e03a28270bbf4870e69c111574d4f3b38159779
Anthony Avila <anthony.p.avila@gmail.com>
2021-03-28 21:32:54 +0900

    inadvertently solved First Repeated Character exercise

---
+++ src/com/DSA/AboutHashTable.java
@@ -0,0 +1,39 @@
+package com.DSA;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AboutHashTable {
+    public static void main(String[] args) {
+        // Key: Employee Number (int)
+        // Value: Employee's Name
+        Map<Integer, String> map = new HashMap<>(); // Map is an interface, HashMap is an implementation
+
+        map.put(1, "George");
+        map.put(2, "Mary");
+        map.put(3, "Bobe");
+        System.out.println(map);
+        map.put(3 ,"Kyle");
+        System.out.println(map);
+        map.put(3 ,"Kyle");
+        map.put(null, null); // perfedctly fine, but no known use cases
+        System.out.println(map);
+        map.remove(null);
+        System.out.println(map);
+        var value = map.get(3);
+        System.out.println(value);
+
+        System.out.println(map.containsKey(1)); // O(1), simply looks up location in Hash Map and returns true or false
+        System.out.println(map.containsValue("Bobe")); // O(n), Hash function has to iterate through all objects and compare value with argument
+        System.out.println(map.containsValue("Kyle"));
+
+        // all of the below lists are iterable
+        System.out.println(map.keySet());
+        System.out.println(map.entrySet());
+        System.out.println(map.values());
+
+        for (var item: map.entrySet())
+            System.out.println(item.getKey()); // can get key, value, set value, etc.
+    }
+
+}
--- src/com/DSA/Main.java
+++ src/com/DSA/Main.java
@@ -4,35 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
+
     public static void main(String[] args) {
-        // Key: Employee Number (int)
-        // Value: Employee's Name
-        Map<Integer, String> map = new HashMap<>(); // Map is an interface, HashMap is an implementation
+        System.out.println(firstRepeatedCharacter());
+    }
 
-        map.put(1, "George");
-        map.put(2, "Mary");
-        map.put(3, "Bobe");
-        System.out.println(map);
-        map.put(3 ,"Kyle");
-        System.out.println(map);
-        map.put(3 ,"Kyle");
-        map.put(null, null); // perfedctly fine, but no known use cases
-        System.out.println(map);
-        map.remove(null);
-        System.out.println(map);
-        var value = map.get(3);
-        System.out.println(value);
+    public static char firstRepeatedCharacter() {
+        String testLetters = "a green apple";
+        Map<Integer, Character> mapped = new HashMap<>();
 
-        System.out.println(map.containsKey(1)); // O(1), simply looks up location in Hash Map and returns true or false
-        System.out.println(map.containsValue("Bobe")); // O(n), Hash function has to iterate through all objects and compare value with argument
-        System.out.println(map.containsValue("Kyle"));
+        int i;
+        char test = 0;
+        for (i = 0; i < testLetters.length(); i++) {
+            if(mapped.containsValue(testLetters.charAt(i))) {
+                test = testLetters.charAt(i);
+                break;
+            }
+            char c = testLetters.charAt(i);
+            mapped.put(i, c);
+        }
 
-        // all of the below lists are iterable
-        System.out.println(map.keySet());
-        System.out.println(map.entrySet());
-        System.out.println(map.values());
-
-        for (var item: map.entrySet())
-            System.out.println(item.getKey()); // can get key, value, set value, etc.
+        System.out.println(mapped);
+        return test;
     }
 }
