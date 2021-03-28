7104a6d859a68dc684245838b7ef3cf8b7b07f29
Anthony Avila <anthony.p.avila@gmail.com>
2021-03-28 12:04:27 +0900

    properly implement Priority Queue with an Array and edge cases

--- src/com/DSA/Main.java
+++ src/com/DSA/Main.java
@@ -2,9 +2,25 @@ package com.DSA;
 
 public class Main {
     public static void main(String[] args) {
-        PriorityQueue test = new PriorityQueue();
-        System.out.println(test);
-        test.insert(2);
-        System.out.println(test);
+        PriorityQueue queue = new PriorityQueue();
+        System.out.println(queue);
+        queue.insert(4);
+        queue.insert(1);
+        System.out.println(queue);
+        queue.insert(5);
+        queue.insert(3);
+        queue.insert(7);
+        System.out.println(queue);
+        System.out.println(queue.remove());
+        System.out.println(queue);
+        queue.insert(10);
+        System.out.println(queue);
+
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        System.out.println(queue.isEmpty());
     }
 }
--- src/com/DSA/PriorityQueue.java
+++ src/com/DSA/PriorityQueue.java
@@ -3,28 +3,39 @@ package com.DSA;
 import java.util.Arrays;
 
 public class PriorityQueue {
-    private int[] priorityQueue = new int[]{1, 3, 5, 7};
+    private final int[] itemsInQueue = new int[5];
+    private int count; // to count the number of items in our queue
 
     // Exercise: Building a Priority Queue using an Array
     public void insert(int item) {
-        int[] arrCopy = new int[priorityQueue.length + 1];
-        var reference = 0;
-        for(int i = priorityQueue.length - 1; i >= 0; i--) {
-            if (item >= priorityQueue[i]) {
-                arrCopy[i + 1] = item;
-                arrCopy[i] = priorityQueue[i];
+        if (count == itemsInQueue.length ) throw new IllegalStateException(); // a further challnege is to resize this array
+
+        int i; // initialize the iterator variable outside the for loop so we don't lose reference to current item
+        for(i = count - 1; i >= 0; i--) {
+            // if item is greater than value at iteration then we want to shift current iteration item to te right
+            if (itemsInQueue[i] > item ) {
+                itemsInQueue[i + 1] = itemsInQueue[i];
             }
             else {
-                arrCopy[i] = reference;
-                arrCopy[i + 1] = priorityQueue[i];
-                reference = priorityQueue[i];
+                break;
             }
         }
-        priorityQueue = arrCopy;
+        itemsInQueue[i + 1] = item; // we know where to insert new item based on when above for loop stopped
+        count++; // increase count before next insert()
+    }
+
+    public int remove() {
+        if (count == 0) throw new IllegalStateException();
+
+        return itemsInQueue[--count]; // removes items at end of queue in scenario where high numbers have higher priority for being processed first
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
     }
 
     @Override
     public String toString() {
-        return Arrays.toString(priorityQueue);
+        return Arrays.toString(itemsInQueue);
     }
 }
